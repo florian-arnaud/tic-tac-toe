f6faf25a4ffd17dc80b06a425d1d561b6ef0fdf9
florian-arnaud <florian.arnaud@laplateforme.io>
2024-11-07 13:57:50 +0100

    Board and keys working

--- main.py
+++ main.py
@@ -11,4 +11,57 @@ def printGameBoard():
       print("", gameBoard[indexRows][indexColumns], end=" |") #end permet de remplacer le saut de ligne après un print par le caractère de notre choix, ici | pour formater la grille.
   print("\n|---|---|---|")
 
+def modifyArray(number, turn):
+  number -= 1
+  if(number == 0):
+    gameBoard[0][0] = turn
+  elif(number == 1):
+    gameBoard[0][1] = turn
+  elif(number == 2):
+    gameBoard[0][2] = turn
+  elif(number == 3):
+    gameBoard[1][0] = turn
+  elif(number == 4):
+    gameBoard[1][1] = turn
+  elif(number == 5):
+    gameBoard[1][2] = turn
+  elif(number == 6):
+    gameBoard[2][0] = turn
+  elif(number == 7):
+    gameBoard[2][1] = turn
+  elif(number == 8):
+    gameBoard[2][2] = turn
+
+def checkSlot(number):
+    number -= 1  
+    row = number // 3
+    col = number % 3
+    if (isinstance(gameBoard[row][col], int)):
+        return True
+    else:
+        print("La case est déjà occupée.")
+
+def switchPlayer(currentPlayer):
+    if currentPlayer == "X":
+        return "O"
+    else:
+        return "X"
+
+currentPlayer = "X"
+turns = 0
+
+while turns < 9:
+    printGameBoard()
+    slotNumber = int(input(f"(Tour de {currentPlayer}) Saisissez un nombre entre 1 et 9: "))
+    if(slotNumber >=1 and slotNumber <=9 and checkSlot(slotNumber)):
+        modifyArray(slotNumber, currentPlayer)
+        turns +=1
+        currentPlayer = switchPlayer(currentPlayer)
+    else:
+        print("Erreur: La case est déjà utilisée ou vous n'avez pas sélectionné une valeur entre 1 et 9")
+
 printGameBoard()
+print("Partie terminée !")
+
+
+
