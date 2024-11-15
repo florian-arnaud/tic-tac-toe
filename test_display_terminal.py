5553f8f8caafb14de4d1cfdc9391d4ed9e1371bc
mohammed-zelmati <mohammed.zelmati@laplateforme.io>
2024-11-15 20:52:16 +0100

    Modifier le fichier test_display-terminal.py

--- test_display_terminal.py
+++ test_display_terminal.py
@@ -1,4 +1,3 @@
-# instructions to the players and demonstration of the scheme with the numbers that must be return
 #les instructions aux joueurs et démonstration de schéma avec les nombres qui doivent les rentrer
 instructions = """ This will be our tic tac toe board
 
@@ -13,14 +12,11 @@ instructions = """ This will be our tic tac toe board
 2. You must fill all 9 spots to get result,
 3. Player 1 will go first
 """
-
-sign_dict =[] # Creating an empty list (création une liste vide) 
-#loop to add a max of 9 values on sign_dict 
+sign_dict =[] # création une liste vide 
 #boucle pour additionner max 9 valeurs sur sign_dict
 for i in range(9):
     sign_dict.append("")
 
-# Interface diagram function displayed on the terminal 
 # fonction de schéma d'interface affiché sur le terminal
 def print_board(sign_dict):
     board =f"""
@@ -32,9 +28,8 @@ def print_board(sign_dict):
          """  
     print(board)
 
-index_list = [] # Creating an empty list 
+index_list = [] # Création une liste vide
 
-# creation of a function that asks players to type numbers between 1 and 9, and assign its numbers to the index_list
 # création d'une fonction qui demande aux joueurs de taper des nombres entre 1 et 9, et affecter ses nombres à la liste index_list
 def take_input(player_name):
     while True :
@@ -51,7 +46,6 @@ def take_input(player_name):
         except ValueError:
             print("Entrée invalide,Veuillez entrer un entier") 
 
-# creation of the function that designates the winner
 # création la fonction qui désigne le vainqueur  
 def result_winner(sign_dict, player_one, player_two):
     winning_combinations = [
@@ -67,15 +61,13 @@ def result_winner(sign_dict, player_one, player_two):
         elif all(sign_dict[i] == 'O' for i in combo):
             print(f"Congratulations {player_two}. You WON.\n Vous avez gagné.!!")
             quit("Thank you both for joining")
-    
     return
-#creation The function that manages the game and orders the players and declares the winner or the game is tie
+
 # création la fonction qui gère le jeu et qui ordonne les joueurs et qui déclare le vainqueur ou le jeu est à égalité
 def main():
     print("Welcome to GAME :|=== TIC TAC TOE ===|")
     player_one = input("Enter player 1 name : ")
     player_two = input("Enter player 2 name : ")
-
     print(f"Thank you for joining Mr./Mrs. {player_one}  and Mr./Mrs. : {player_two}")
     print(instructions)
     print(f"Mr. {player_one}'s sign is - X")
@@ -93,6 +85,5 @@ def main():
         result_winner(sign_dict, player_one, player_two)
     print("This is a tie..!! No body won. Play Again.")
     
-# The call of the main() function to execute the game
 # L'appelle de la fonction main() pour éxecute le jeu
 main()   
