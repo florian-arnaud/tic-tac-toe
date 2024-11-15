5fff8817e295195813d95efc1f51bc04561cee26
mohammed-zelmati <mohammed.zelmati@laplateforme.io>
2024-11-15 20:38:58 +0100

    Modifier et simplifier le fichier test_display_terminal.py

--- test_display_terminal.py
+++ test_display_terminal.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> 13dac98797cc4186ba017caaada2d8efad4fd3c0
-# ====// Mohammed ZELMATI \\====
-
 # instructions to the players and demonstration of the scheme with the numbers that must be return
 #les instructions aux joueurs et démonstration de schéma avec les nombres qui doivent les rentrer
 instructions = """ This will be our tic tac toe board
@@ -44,80 +38,37 @@ index_list = [] # Creating an empty list
 # création d'une fonction qui demande aux joueurs de taper des nombres entre 1 et 9, et affecter ses nombres à la liste index_list
 def take_input(player_name):
     while True :
-        x  = int(input(f'{player_name} : '))
-        x -= 1
-        if 0 <= x < 10:
-            if x in index_list:
-                print("this spot is blocked.")
-                continue
-            index_list.append(x)
-            return x
-        print("Please enter number between 1-9")
+        try:
+            x  = int(input(f'{player_name} : '))
+            x -= 1
+            if 0 <= x < 10:
+                if x in index_list:
+                    print("this spot is blocked.")
+                    continue
+                index_list.append(x)
+                return x
+            print("Please enter number between 1-9")
+        except ValueError:
+            print("Entrée invalide,Veuillez entrer un entier") 
 
-#Check if one of the three row contain the X or O sign in each slot
-def horizontal_win():
-    if (sign_dict[0] == "X" and sign_dict[1] == "X" and sign_dict[2] == "X")
-    or (sign_dict[3] == "X" and sign_dict[4] == "X" and sign_dict[5] == "X")
-    or (sign_dict[6] == "X" and sign_dict[7] == "X" and sign_dict[8] == "X"):
-        print("Player 1 won !")
-
-    elif (sign_dict[0] == "O" and sign_dict[1] == "O" and sign_dict[2] == "O")
-    or (sign_dict[3] == "O" and sign_dict[4] == "O" and sign_dict[5] == "O")
-    or (sign_dict[6] == "O" and sign_dict[7] == "O" and sign_dict[8] == "O"):
-        print("Player 2 won !")
-
-    else:
-        return
-
-def vertical_win():
-    if (sign_dict[0] == "X" and sign_dict[3] == "X" and sign_dict[6] == "X")
-    or (sign_dict[1] == "X" and sign_dict[4] == "X" and sign_dict[7] == "X")
-    or (sign_dict[2] == "X" and sign_dict[5] == "X" and sign_dict[8] == "X"):
-        print("Player 1 won !")
-
-    elif (sign_dict[0] == "O" and sign_dict[3] == "O" and sign_dict[6] == "O")
-    or (sign_dict[1] == "O" and sign_dict[4] == "O" and sign_dict[7] == "O")
-    or (sign_dict[2] == "O" and sign_dict[5] == "O" and sign_dict[8] == "O")
-        print("Player 2 won !")
-
-    else:
-        return
-def diagonal_win():
-    if (sign_dict[0] == "X" and sign_dict[4] == "X" and sign_dict[8] == "X") 
-    or (sign_dict[2] == "X" and sign_dict[4] == "X" and sign_dict[6] == "X"):
-        print("Player 1 won !")
-
-    elif (sign_dict[0] == "O" and sign_dict[4] == "O" and sign_dict[8] == "O") 
-    or (sign_dict[2] == "O" and sign_dict[4] == "O" and sign_dict[6] == "O"):
-        print("Player 2 won !")
-    else:
-        return
 # creation of the function that designates the winner
-# création la fonction qui désigne le vainqueur 
+# création la fonction qui désigne le vainqueur  
 def result_winner(sign_dict, player_one, player_two):
-    if sign_dict[0] == sign_dict[1] == sign_dict[2] == 'X'\
-        or sign_dict[3] == sign_dict[4] == sign_dict[5] == 'X'\
-        or sign_dict[6] == sign_dict[7] == sign_dict[8] == 'X'\
-        or sign_dict[0] == sign_dict[3] == sign_dict[6] == 'X'\
-        or sign_dict[1] == sign_dict[4] == sign_dict[7] == 'X'\
-        or sign_dict[2] == sign_dict[5] == sign_dict[8] == 'X'\
-        or sign_dict[0] == sign_dict[4] == sign_dict[8] == 'X'\
-        or sign_dict[2] == sign_dict[4] == sign_dict[6] == 'X' :
-        print(f"Congratulations {player_one}. You WON.!!")
-        quit("Thank you both for joining")
-        
-    elif sign_dict[0] == sign_dict[1] == sign_dict[2] == 'O'\
-        or sign_dict[3] == sign_dict[4] == sign_dict[5] == 'O'\
-        or sign_dict[6] == sign_dict[7] == sign_dict[8] == 'O'\
-        or sign_dict[0] == sign_dict[3] == sign_dict[6] == 'O'\
-        or sign_dict[1] == sign_dict[4] == sign_dict[7] == 'O'\
-        or sign_dict[2] == sign_dict[5] == sign_dict[8] == 'O'\
-        or sign_dict[0] == sign_dict[4] == sign_dict[8] == 'O'\
-        or sign_dict[2] == sign_dict[4] == sign_dict[6] == 'O' :
-        print("Congratulations {player_two}. You WON.!!")
-        quit("Thank you both for joining")
-    else:
-        return
+    winning_combinations = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],  # lignes horizontales
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],  # lignes verticales
+        [0, 4, 8], [2, 4, 6]              # diagonales
+    ]
+    
+    for combo in winning_combinations:
+        if all(sign_dict[i] == 'X' for i in combo):
+            print(f"Congratulations {player_one}. You WON.!!")
+            quit("Thank you both for joining")
+        elif all(sign_dict[i] == 'O' for i in combo):
+            print(f"Congratulations {player_two}. You WON.\n Vous avez gagné.!!")
+            quit("Thank you both for joining")
+    
+    return
 #creation The function that manages the game and orders the players and declares the winner or the game is tie
 # création la fonction qui gère le jeu et qui ordonne les joueurs et qui déclare le vainqueur ou le jeu est à égalité
 def main():
